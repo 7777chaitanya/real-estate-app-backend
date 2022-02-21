c5b62431c4d4b64066e89801f6259e830eec8214
Chaitanya7666 <chaitu.y25@gmail.com>
2022-02-21 23:59:13 +0530

    added create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def create
+    puts "asd" 
+    # @user = User.new(user_params)
+    # if @user.save
+    #   render :json => {:username => @user.username}
+  end
+end
